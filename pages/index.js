b48f37295b17f570e3b645c5b8299f603c8972a9
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-07-25 15:50:41 +0700

    ADD nextjs

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import { useState } from 'react'
+import { Button } from 'semantic-ui-react'
+import styles from '../styles/Home.module.css'
+import getLongText from '../utils/text'
+
+export default function Home() {
+	const [text, setText] = useState(getLongText(20))
+
+	const moreText = () => {
+		const moreText = text + getLongText(10)
+		setText(moreText)
+	}
+
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Just Scroll</title>
+				<meta name="description" content="Just for scrolling" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className={styles.main}>
+				<h1 className={styles.headerText}>
+					Did you just bought MX Master 3 with MagSpeed™ Electromagnetic scrolling and want to try? GO FOR
+					IT!!
+				</h1>
+				<div className={styles.scrollText}>{text}</div>
+				<Button basic color="red" content="Red" onClick={moreText}>
+					GIVE ME MORE!!
+				</Button>
+			</div>
+		</div>
+	)
+}
